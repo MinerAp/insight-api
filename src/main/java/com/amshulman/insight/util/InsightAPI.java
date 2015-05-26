3500738672e88a10c32a7989ff145e0caef92688
Andrew Shulman <andy.shulman@hotmail.com>
2015-05-25 23:21:17 -0400

    Add method to register an action with Insight

--- src/main/java/com/amshulman/insight/util/InsightAPI.java
+++ src/main/java/com/amshulman/insight/util/InsightAPI.java
@@ -2,9 +2,12 @@ package com.amshulman.insight.util;
 
 import javax.annotation.Nonnull;
 
+import com.amshulman.insight.action.InsightAction;
 import com.amshulman.insight.row.RowEntry;
 
 public interface InsightAPI {
 
     public void logEvent(@Nonnull RowEntry row);
+
+    public void registerAction(@Nonnull InsightAction action);
 }
