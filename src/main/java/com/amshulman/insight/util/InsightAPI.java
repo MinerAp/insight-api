342f8f1fb425f54e7a71beea1c543ae258bff570
Andrew Shulman <andy.shulman@hotmail.com>
2015-05-26 01:38:34 -0400

    Expose the API as a Plugin

--- src/main/java/com/amshulman/insight/util/InsightAPI.java
+++ src/main/java/com/amshulman/insight/util/InsightAPI.java
@@ -2,10 +2,12 @@ package com.amshulman.insight.util;
 
 import javax.annotation.Nonnull;
 
+import org.bukkit.plugin.Plugin;
+
 import com.amshulman.insight.action.InsightAction;
 import com.amshulman.insight.row.RowEntry;
 
-public interface InsightAPI {
+public interface InsightAPI extends Plugin {
 
     public void logEvent(@Nonnull RowEntry row);
 
