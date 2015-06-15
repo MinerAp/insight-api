d3fe8bc614d27707ab70b4035990bdb2b75c7774
Andrew Shulman <andy.shulman@hotmail.com>
2015-06-14 20:15:54 -0400

    Add documentation for RollbackActionStatus

--- src/main/java/com/amshulman/insight/action/InsightAction.java
+++ src/main/java/com/amshulman/insight/action/InsightAction.java
@@ -6,7 +6,7 @@ public abstract class InsightAction {
 
     public abstract String getName();
 
-    public abstract  String getFriendlyDescription();
+    public abstract String getFriendlyDescription();
 
     public abstract <T extends InsightAction> RollbackAction<T> getRollbackAction();
 
@@ -15,9 +15,24 @@ public abstract class InsightAction {
     }
 
     public enum RollbackActionStatus {
+        /*
+         * If the rollback operation succeeded
+         */
         OK,
+
+        /*
+         * If the rollback is unnecessary
+         */
         SKIPPED,
-        NO_ACTION_AVAILABLE,
+
+        /*
+         * If the rollback operation was impossible
+         */
         FAILED,
+
+        /*
+         * If the plugin that registered the action is not available. Plugins should not return this status.
+         */
+        NO_ACTION_AVAILABLE,
     }
 }
